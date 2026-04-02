efa96f6733cb8a7756b2c84f0452c3879b64e87c
DaviMoreira27 <davimsantana2706@gmail.com>
2026-04-02 00:01:47 -0300

    feat: initiated chat and embedding models

--- app/config.py
+++ app/config.py
@@ -21,6 +21,11 @@ class ChatModel(Enum):
     GEMINI_2_5_PRO = "gemini-2.5-pro"
     GEMINI_2_FLASH = "gemini-2.0-flash"
 
+class EmbeddingModels (Enum):
+    # Google
+    GEMINI_001 = "gemini-embedding-001"
+
+
 ANTHROPIC_MODELS = {
     ChatModel.CLAUDE_SONNET_4_6,
     ChatModel.CLAUDE_HAIKU_4_5,
@@ -31,11 +36,12 @@ GOOGLE_MODELS = {
     ChatModel.GEMINI_2_5_FLASH,
     ChatModel.GEMINI_2_5_PRO,
     ChatModel.GEMINI_2_FLASH,
+    EmbeddingModels.GEMINI_001
 }
 
 class Config:
     @staticmethod
-    def get_model_config(model: ChatModel) -> ModelConfig:
+    def get_model_config(model: ChatModel | EmbeddingModels) -> ModelConfig:
         if model in ANTHROPIC_MODELS:
             api_key = os.getenv("ANTHROPIC_API_KEY")
             if api_key is None:
--- app/modules/engine/service.py
+++ app/modules/engine/service.py
@@ -1,10 +1,20 @@
-from app.config import ChatModel, Config
+from app.config import ChatModel, Config, EmbeddingModels
 from langchain.chat_models import init_chat_model
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
 
 class RagEngine:
-    def __init__(self, config_service: Config):
+    def __init__(
+        self,
+        config_service: Config,
+        collection_name: str
+    ):
        self.config_service = config_service
 
-       api_key = self.config_service.get_model_config(ChatModel.CLAUDE_HAIKU_4_5).api_key
-
-       self.chat_model = init_chat_model(api_key)
+       self.chat_model = init_chat_model(model=str(ChatModel.CLAUDE_HAIKU_4_5))
+       self.embedding_model = GoogleGenerativeAIEmbeddings(model=f"models/{EmbeddingModels.GEMINI_001}")
+       self.vector_store = Chroma(
+           collection_name=collection_name,
+           embedding_function=self.embedding_model,
+           persist_directory="./chroma_langchain_db",
+       )
